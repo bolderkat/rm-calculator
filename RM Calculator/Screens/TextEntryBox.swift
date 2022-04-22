4f7bdd3fed68c80dd16595fc2e5f4b8b1f00e0eb
Daniel Luo <dluo@squareup.com>
2022-04-22 09:44:21 -0700

    Add outline around textfield

--- RM Calculator/Screens/TextEntryBox.swift
+++ RM Calculator/Screens/TextEntryBox.swift
@@ -9,6 +9,17 @@ import BlueprintUI
 import BlueprintUICommonControls
 
 struct TextEntryBox: ProxyElement {
+    private enum Metrics {
+        static let inset: UIEdgeInsets = .init(
+            top: 0.0,
+            left: 10.0,
+            bottom: 0.0,
+            right: 0.0
+        )
+        static let outlineCornerRadius: CGFloat = 10.0
+        static let outlineWidth: CGFloat = 0.5
+    }
+    
     var placeholder: String
     var entry: String
     var didUpdateEntry: (String) -> Void
@@ -20,5 +31,20 @@ struct TextEntryBox: ProxyElement {
             textField.clearButtonMode = .whileEditing
             textField.onChange = didUpdateEntry
         }
+        .inset(by: Metrics.inset)
+        .box(
+            background: .clear,
+            corners: .rounded(
+                radius: Metrics.outlineCornerRadius,
+                corners: .all
+            ),
+            cornerCurve: .circular,
+            borders: .solid(
+                color: .lightGray,
+                width: Metrics.outlineWidth
+            ),
+            shadow: .none,
+            clipsContent: false
+        )
     }
 }
